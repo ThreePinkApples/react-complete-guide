23112904d082ea47ab47f281e2026a3fc0af7783
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-06 16:14:23 +0200

    Forgot css

--- src/components/ExpenseItemList/ExpenseItemList.js
+++ src/components/ExpenseItemList/ExpenseItemList.js
@@ -1,9 +1,10 @@
+import "./ExpenseItemList.css";
 import ExpenseItem from "./ExpenseItem";
 
 function ExpenseItemList(props) {
   const items = props.expenses;
   return (
-    <div>
+    <div className="expense-item-list">
       {items
         .sort((a, b) => (a.date < b.date ? 1 : -1))
         .map((item) => (
