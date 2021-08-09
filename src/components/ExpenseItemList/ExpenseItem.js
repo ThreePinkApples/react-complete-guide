80a25a5c1f7986cec6d49a272663704a4cb255b6
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-09 15:55:48 +0200

    Card wrapper

--- src/components/ExpenseItemList/ExpenseItem.js
+++ src/components/ExpenseItemList/ExpenseItem.js
@@ -1,16 +1,17 @@
 import "./ExpenseItem.css";
+import Card from "../Wrappers/Card/Card";
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
   const item = props.item;
   return (
-    <div key={item.id} id={item.id} className="expense-item">
+    <Card key={item.id} id={item.id} className="expense-item">
       <ExpenseDate date={item.date} />
       <div className="expense-item__description">
         <h2>{item.title}</h2>
         <div className="expense-item__price">${item.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
--- src/components/ExpenseItemList/ExpenseItemList.js
+++ src/components/ExpenseItemList/ExpenseItemList.js
@@ -1,16 +1,18 @@
 import "./ExpenseItemList.css";
+import Card from "../Wrappers/Card/Card";
 import ExpenseItem from "./ExpenseItem";
+
 
 function ExpenseItemList(props) {
   const items = props.expenses;
   return (
-    <div className="expense-item-list">
+    <Card className="expense-item-list">
       {items
         .sort((a, b) => (a.date < b.date ? 1 : -1))
         .map((item) => (
           <ExpenseItem item={item} />
         ))}
-    </div>
+    </Card>
   );
 }
 
