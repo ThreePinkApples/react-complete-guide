7ae5e89a119624ab55af87baf83d0ccdb5446abc
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-24 09:52:57 +0200

    Double quote strings

--- Login/src/App.js
+++ Login/src/App.js
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
