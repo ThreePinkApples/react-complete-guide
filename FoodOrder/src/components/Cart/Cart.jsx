b3b5e85310ff2798328fa7cbd3bea40791b3e330
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-24 16:12:37 +0200

    Proper add to/remove from cart

--- FoodOrder/src/components/Cart/Cart.jsx
+++ FoodOrder/src/components/Cart/Cart.jsx
@@ -2,26 +2,54 @@ import { useContext } from "react";
 import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal/Modal";
 import styles from "./Cart.module.css";
+import CartItem from "./CartItem/CartItem";
 
 export default function Cart(props) {
   const cartContext = useContext(CartContext);
   const items = cartContext.items || [
     { id: "c1", name: "Barbecue Sushi", amount: 4, price: 15.99 },
   ];
-  const cartListItems = items.map((item) => <li>{item.name}</li>);
+  const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
+  const hasItems = items.length > 0;
+
+  const addToCart = (item) => {
+      cartContext.addItem({
+          ...item,
+          amount: 1
+      })
+  }
+
+  const removeFromCart = (itemId) => {
+      cartContext.removeItem(itemId);
+  }
+
+  const cartListItems = items.map((item) => (
+    <CartItem
+      key={item.id}
+      name={item.name}
+      price={item.price}
+      amount={item.amount}
+      onAdd={addToCart.bind(null, item)}
+      onRemove={removeFromCart.bind(null, item.id)}
+    >
+      {item.name}
+    </CartItem>
+  ));
 
   return (
     <Modal onBackdropClick={props.onClose}>
       <ul className={styles["cart-items"]}>{cartListItems}</ul>
       <div className={styles.total}>
         <span>Total Amount</span>
-        <span>{15.99 * 4}</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={styles.actions}>
         <button className={styles["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={styles.button}>Order</button>
+        <button className={styles.button} disabled={!hasItems}>
+          Order
+        </button>
       </div>
     </Modal>
   );
--- FoodOrder/src/store/CartProvider.jsx
+++ FoodOrder/src/store/CartProvider.jsx
@@ -9,14 +9,50 @@ const DEFAULT_CART_STATE = {
 const cartReducer = (prevState, action) => {
   if (action.type === "ADD_ITEM") {
     const newItem = action.item;
-    const updatedItems = prevState.items.concat(newItem);
-    const newTotalAmount = prevState.totalAmount + (newItem.price * newItem.amount)
-    return {
-        items: updatedItems,
-        totalAmount: newTotalAmount
+    const newTotalAmount =
+      prevState.totalAmount + newItem.price * newItem.amount;
+    const existingIndex = prevState.items.findIndex(
+      (item) => item.id === newItem.id
+    );
+    const existingItem = prevState.items[existingIndex];
+    let updatedItems;
+    if (existingItem) {
+      const updatedItem = {
+        ...existingItem,
+        amount: existingItem.amount + newItem.amount,
+      };
+      updatedItems = [...prevState.items];
+      updatedItems[existingIndex] = updatedItem;
+    } else {
+      updatedItems = prevState.items.concat(newItem);
     }
+    return {
+      items: updatedItems,
+      totalAmount: newTotalAmount,
+    };
   } else if (action.type === "REMOVE_ITEM") {
-      const itemId = action.itemId;
+    const itemId = action.itemId;
+    const existingIndex = prevState.items.findIndex(
+      (item) => item.id === itemId
+    );
+    const existingItem = prevState.items[existingIndex];
+    if (existingItem) {
+      let updatedItems = [...prevState.items];
+      const newTotalAmount = prevState.totalAmount - existingItem.price;
+      if (existingItem.amount === 1) {
+        updatedItems.splice(existingIndex, 1);
+      } else {
+        const updatedItem = {
+          ...existingItem,
+          amount: existingItem.amount - 1,
+        };
+        updatedItems[existingIndex] = updatedItem;
+      }
+      return {
+        items: updatedItems,
+        totalAmount: newTotalAmount,
+      };
+    }
   }
   return DEFAULT_CART_STATE;
 };
