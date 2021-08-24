c14a7954429642c1f55f38e006b95f6666f23054
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-24 16:22:46 +0200

    Add animation to cart button

--- FoodOrder/src/components/Layout/Header/HeaderCartButton.jsx
+++ FoodOrder/src/components/Layout/Header/HeaderCartButton.jsx
@@ -1,15 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../../store/cart-context";
 import CartIcon from "../../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 
 export default function HeaderCartButton(props) {
+  const [buttonIsAnimating, setButtonIsAnimating] = useState(false);
   const cartContext = useContext(CartContext);
   const numberOfItems = cartContext.items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
+
+  const buttonClasses = `${styles.button} ${
+    buttonIsAnimating ? styles.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartContext.totalAmount === 0) {
+      return;
+    }
+    setButtonIsAnimating(true);
+    const timer = setTimeout(() => {
+      setButtonIsAnimating(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartContext.totalAmount]);
+
   return (
-    <button className={styles.button} onClick={props.openCart}>
+    <button className={buttonClasses} onClick={props.openCart}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
