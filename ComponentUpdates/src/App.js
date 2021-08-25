368153094fc50b5db200932d37b2ccad2241234f
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-25 12:27:22 +0200

    Conditionally show paragraph

--- ComponentUpdates/src/App.js
+++ ComponentUpdates/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-
-import './App.css';
+import { useState } from "react";
+import Button from "./components/UI/Button/Button";
+import "./App.css";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraph = () => {
+    setShowParagraph(prevState => !prevState);
+  }
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <Button onClick={toggleParagraph}>Toggle paragraph!</Button>
+      {showParagraph && <p>This is new!</p>}
     </div>
   );
 }
