70583d60a56c06413aa1f167000fe3a4669880f4
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-26 15:07:42 +0200

    SimpleInput validate on submit

--- AdvancedFormsAndInput/src/components/SimpleInput.js
+++ AdvancedFormsAndInput/src/components/SimpleInput.js
@@ -1,12 +1,41 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameRef = useRef();
+  const [name, setName] = useState("");
+  const [nameIsValid, setNameIsValid] = useState(true);
+
+  function onNameChange(event) {
+    setName(event.target.value);
+  }
+
+  function submitForm(event) {
+    event.preventDefault();
+    if (name.trim() === "") {
+      setNameIsValid(false);
+      console.log("No!");
+      return;
+    }
+    setNameIsValid(true);
+    console.log(name);
+    console.log(nameRef.current.value);
+  }
+
   return (
-    <form>
-      <div className="form-control">
+    <form onSubmit={submitForm}>
+      <div className={`form-control ${!nameIsValid ? "invalid" : ""}`}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={onNameChange}
+          ref={nameRef}
+        />
+        {!nameIsValid && <p className="error-text">Name must not be empty</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </div>
     </form>
   );
