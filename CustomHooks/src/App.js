20ec7525166c505fe93663f9306bcfc693d2f21f
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-26 12:32:30 +0200

    Add CustomHooks starter project

---
+++ CustomHooks/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BackwardCounter from "./components/BackwardCounter";
+import ForwardCounter from "./components/ForwardCounter";
+function App() {
+  return (
+    <>
+      <ForwardCounter />
+      <BackwardCounter />
+    </>
+  );
+}
+
+export default App;
