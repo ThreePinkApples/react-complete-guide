d0e5387ee4e94c6a889e0f13f75df9aecf7c123f
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-26 09:47:27 +0200

    Basic fetch error handling

--- Movies/src/App.js
+++ Movies/src/App.js
@@ -5,22 +5,32 @@ import MoviesList from "./components/MoviesList";
 function App() {
   const [moviesLoading, setMoviesLoading] = useState(false);
   const [movies, setMovies] = useState([]);
+  const [error, setError] = useState("");
 
   async function fetchMovies() {
+    setError("");
     setMoviesLoading(true);
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    setMovies(
-      data.results.map((movie) => {
-        return {
-          id: movie.episode_id,
-          title: movie.title,
-          releaseDate: movie.release_date,
-          openingText: movie.opening_crawl,
-        };
-      })
-    );
-    setMoviesLoading(false);
+    try {
+      const response = await fetch("https://swapi.dev/api/film/");
+      if (!response.ok) {
+        throw new Error("Response error " + response.status);
+      }
+      const data = await response.json();
+      setMovies(
+        data.results.map((movie) => {
+          return {
+            id: movie.episode_id,
+            title: movie.title,
+            releaseDate: movie.release_date,
+            openingText: movie.opening_crawl,
+          };
+        })
+      );
+    } catch(error) {
+      setError(error.message);
+    } finally {
+      setMoviesLoading(false);
+    }
   }
 
   return (
@@ -30,7 +40,8 @@ function App() {
       </section>
       <section>
         {moviesLoading && <p>Fetching movies...</p>}
-        {!moviesLoading && <MoviesList movies={movies} />}
+        {!moviesLoading && !error && <MoviesList movies={movies} />}
+        {!moviesLoading && error && <p>{error}</p>}
       </section>
     </>
   );
