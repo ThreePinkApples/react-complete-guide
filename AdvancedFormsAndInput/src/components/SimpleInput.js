02220a27a128c69489cbde211eb135f91bb74476
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-26 15:25:57 +0200

    Simplify nameIsValid and reset on submit

--- AdvancedFormsAndInput/src/components/SimpleInput.js
+++ AdvancedFormsAndInput/src/components/SimpleInput.js
@@ -3,33 +3,27 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const nameRef = useRef();
   const [name, setName] = useState("");
-  const [nameIsValid, setNameIsValid] = useState(false);
   const [nameTouched, setNameTouched] = useState(false);
+  const nameIsValid = nameTouched && name.trim() !== "";
   const showError = nameTouched && !nameIsValid;
-
-  const isNameValid = (name) => name.trim() !== "";
 
   function onNameChange(event) {
     setNameTouched(true);
     setName(event.target.value);
-    if (!nameIsValid && showError) {
-      setNameIsValid(isNameValid(event.target.value));
-    }
   }
 
   function onNameBlur(event) {
     setNameTouched(true);
-    setNameIsValid(isNameValid(event.target.value));
   }
 
   function submitForm(event) {
     event.preventDefault();
     setNameTouched(true);
-    if (!isNameValid(name)) {
-      setNameIsValid(false);
+    if (!nameIsValid) {
       return;
     }
-    setNameIsValid(true);
+    setName("");
+    setNameTouched(false);
   }
 
   return (
