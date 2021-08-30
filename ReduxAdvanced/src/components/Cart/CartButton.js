21399cdfe84dd8e0f69d9c2d47d8ba1c2c5f55b7
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-30 13:15:25 +0200

    Use Redux to store cart open/close state

--- ReduxAdvanced/src/components/Cart/CartButton.js
+++ ReduxAdvanced/src/components/Cart/CartButton.js
@@ -1,8 +1,16 @@
+import { useDispatch } from "react-redux";
+import { uiActions } from "../../store/ui-slice";
 import styles from "./CartButton.module.css";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const toggleCart = () => {
+    dispatch(uiActions.toggleCart());
+  };
+
   return (
-    <button className={styles.button}>
+    <button className={styles.button} onClick={toggleCart}>
       <span>My Cart</span>
       <span className={styles.badge}>1</span>
     </button>
