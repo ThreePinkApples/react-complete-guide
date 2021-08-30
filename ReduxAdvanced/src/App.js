0bf2c74f5d4be007b63ffb34cd9edcba2b005f46
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-30 14:33:43 +0200

    PUT cart data to firebase

--- ReduxAdvanced/src/App.js
+++ ReduxAdvanced/src/App.js
@@ -1,13 +1,27 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
+const firebaseUrL =
+  "https://udemyreactmovies-default-rtdb.europe-west1.firebasedatabase.app";
+const cartUrl = `${firebaseUrL}/cart.json`;
+
 function App() {
   const { cartIsVisible } = useSelector((state) => state.ui);
+  const cart = useSelector((state) => state.cart);
+  useEffect(() => {
+    fetch(cartUrl, {
+      method: "PUT",
+      body: JSON.stringify(cart),
+      headers: { "Content-Type": "application/json" },
+    });
+  }, [cart]);
+
   return (
     <Layout>
-      {cartIsVisible && <Cart /> }
+      {cartIsVisible && <Cart />}
       <Products />
     </Layout>
   );
