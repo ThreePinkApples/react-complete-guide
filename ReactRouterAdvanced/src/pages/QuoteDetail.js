bbf341cdee88f4407dda801dbae112ea66afbd21
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-31 14:39:41 +0200

    QuoteDetail - Get comment path from RouteMatch

--- ReactRouterAdvanced/src/pages/QuoteDetail.js
+++ ReactRouterAdvanced/src/pages/QuoteDetail.js
@@ -1,4 +1,4 @@
-import { Route, useParams, Link, Switch } from "react-router-dom";
+import { Route, useParams, Link, Switch, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import NotFound from "./NotFound";
@@ -18,22 +18,22 @@ const DUMMY_QUOTES = [
 
 export default function QuoteDetail() {
   const params = useParams();
+  const routeMatch = useRouteMatch();
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
   if (!quote) {
     return <NotFound />;
   }
-  const commentPath = `/quotes/${quote.id}/comments`;
 
   return (
     <>
       <HighlightedQuote text={quote.text} author={quote.author} />
       <Switch>
-        <Route path={commentPath}>
+        <Route path={`${routeMatch.path}/comments`}>
           <Comments />
         </Route>
         <Route path="*">
           <div className="centered">
-            <Link to={commentPath} className="btn--flat">
+            <Link to={`${routeMatch.url}/comments`} className="btn--flat">
               Load comments
             </Link>
           </div>
