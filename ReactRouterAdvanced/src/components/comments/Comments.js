01df3e4ecb4f8516da97a570e949d3a284f55acc
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-31 16:07:07 +0200

    Load comments from firebase and refresh when adding comment

--- ReactRouterAdvanced/src/components/comments/Comments.js
+++ ReactRouterAdvanced/src/components/comments/Comments.js
@@ -1,13 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useHttp from "../../hooks/use-http";
+import { getAllComments } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
 import styles from "./Comments.module.css";
+import CommentsList from "./CommentsList";
 import NewCommentForm from "./NewCommentForm";
 
 const Comments = (props) => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const { quoteId } = props;
+  const {
+    sendRequest,
+    status,
+    data: loadedComments,
+    error,
+  } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
+
+  const onAddComment = () => {
+    setIsAddingComment(false);
+    sendRequest(quoteId);
+  };
+
+  let comments;
+  if (status === "pending") {
+    comments = (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  } else if (
+    status === "completed" &&
+    loadedComments &&
+    loadedComments.length > 0
+  ) {
+    comments = <CommentsList comments={loadedComments} />;
+  } else if (status === "completed") {
+    comments = <p className="centered">No comments</p>;
+  }
 
   return (
     <section className={styles.comments}>
@@ -17,8 +54,10 @@ const Comments = (props) => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm quoteId={props.quoteId} />}
-      <p>Comments...</p>
+      {isAddingComment && (
+        <NewCommentForm quoteId={quoteId} onAddComment={onAddComment} />
+      )}
+      {comments}
     </section>
   );
 };
