8f66f3a35e495d05e496738363369c8795dd1eff
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-31 09:52:55 +0200

    Test out React Router

--- ReactRouter/src/App.js
+++ ReactRouter/src/App.js
@@ -1,7 +1,21 @@
+import { Route, Switch } from "react-router-dom";
+import Products from "./components/Products/Products";
+import Welcome from "./components/Welcome/Welcome";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Switch>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/">
+          <h1>¯\_(ツ)_/¯</h1>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ ReactRouter/src/components/Welcome/Welcome.js
@@ -0,0 +1,3 @@
+export default function Welcome() {
+  return <h1>The Welcome Page</h1>;
+}
