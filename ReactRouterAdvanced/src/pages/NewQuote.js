57ca34bd2e420edb4a277aa680b138266913b88b
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-31 15:24:00 +0200

    Save new quotes to firebase

--- ReactRouterAdvanced/src/pages/NewQuote.js
+++ ReactRouterAdvanced/src/pages/NewQuote.js
@@ -1,16 +1,27 @@
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 export default function NewQuote() {
   const history = useHistory();
-  const addQuote = (quote) => {
-    history.push("/quotes");
-  }
+  const { sendRequest, status } = useHttp(addQuote);
+
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
+  const onAddQuote = (quote) => {
+    sendRequest(quote);
+  };
 
   return (
     <>
       <h2>Add a quote</h2>
-      <QuoteForm onAddQuote={addQuote} />
+      <QuoteForm onAddQuote={onAddQuote} />
     </>
   );
 }
