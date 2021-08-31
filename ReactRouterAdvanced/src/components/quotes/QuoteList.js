24f174b8f90d87f405c1853ab50a9501c1cb1c46
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-31 14:11:50 +0200

    Add button to toggle quote sort

--- ReactRouterAdvanced/src/components/quotes/QuoteList.js
+++ ReactRouterAdvanced/src/components/quotes/QuoteList.js
@@ -1,12 +1,39 @@
 import { Fragment } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import QuoteItem from "./QuoteItem";
 import styles from "./QuoteList.module.css";
 
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const isSortingAsc = params.get("sort") === "asc";
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAsc);
+
+  const changeSorting = () => {
+    const newSort = isSortingAsc ? "desc" : "asc";
+    history.push("/quotes?sort=" + newSort);
+  };
+
   return (
     <Fragment>
+      <div className={styles.sorting}>
+        <button onClick={changeSorting}>
+          Sort {isSortingAsc ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={styles.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
