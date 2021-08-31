e0675d87439e53cfaef5a5cce14608a4e6921c14
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-31 12:27:09 +0200

    Add some dummy quotes

--- ReactRouterAdvanced/src/pages/Quotes.js
+++ ReactRouterAdvanced/src/pages/Quotes.js
@@ -1,3 +1,23 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Frank Frankensteen",
+    text: "I'm not the \"doctor\" you're looking for",
+  },
+  {
+    id: "q2",
+    author: "Henry Frankenstein",
+    text: "Monster? What monster? I see no monster.",
+  },
+];
+
 export default function Quotes() {
-    return <p>Quote me, why do you?</p>
+    const quotes = DUMMY_QUOTES
+  return (
+    <>
+      <QuoteList quotes={quotes} />
+    </>
+  );
 }
