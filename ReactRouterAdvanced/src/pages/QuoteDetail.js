f7926fddaad49c3e712d5ef1c4568fb80b4f2205
Øyvind Heddeland Instefjord <oyvind.instefjord@knowit.no>
2021-08-31 13:09:44 +0200

    Display quote in QuoteDetail page using HighlitedQuote
    Just dummy data for now.

--- ReactRouterAdvanced/src/pages/QuoteDetail.js
+++ ReactRouterAdvanced/src/pages/QuoteDetail.js
@@ -1,12 +1,31 @@
-import { Route, useParams } from "react-router-dom";
+import { Redirect, Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Frank Frankensteen",
+    text: "I'm not the \"doctor\" you're looking for",
+  },
+  {
+    id: "q2",
+    author: "Henry Frankenstein",
+    text: "Monster? What monster? I see no monster.",
+  },
+];
 
 export default function QuoteDetail() {
   const params = useParams();
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  if (!quote) {
+    return <h2>ಠ_ಠ</h2>;
+  }
+
   return (
     <>
-      <p>{params.quoteId} - Such details</p>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${quote.id}/comments`}>
         <Comments />
       </Route>
     </>
